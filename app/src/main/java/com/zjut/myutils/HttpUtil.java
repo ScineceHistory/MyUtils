bee7dd209c870cac5783ace5e694703d30d547ec
ScienceHistory <980019603@qq.com>
2016-05-25 14:59:59 +0800

    添加一个HttpUtil的网络访问工具类

---
+++ app/src/main/java/com/zjut/myutils/HttpUtil.java
@@ -0,0 +1,111 @@
+package com.zjut.myutils;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by ScienceHistory on 16/4/9.
+ * 网络访问工具类
+ */
+
+public class HttpUtil {
+    /**
+     * HttpURLConnection请求方式
+     * @param address 请求地址
+     * @param listener 回调接口，用于回传响应的数据
+     */
+    public static void sendRequestWithHttpURLConnection(final String address,final HttpCallbackListener listener) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                InputStream in = null;
+                BufferedReader reader = null;
+                try {
+                    URL url = new URL(address);
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setReadTimeout(8000);
+                    connection.setConnectTimeout(8000);
+                    connection.setDoInput(true);
+                    connection.setDoOutput(true);
+                    // 下面对获取到的输入流进行读取
+                    in = connection.getInputStream();
+                    reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    if (listener != null) {
+                        //回调onFinish()方法
+                        listener.onFinish(response.toString());
+                    }
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                    if (listener != null) {
+                        // 回调onError()方法
+                        listener.onError(e);
+                    }
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                    try {
+                        if (in != null) {
+                            in.close();
+                        }
+                        if (reader != null) {
+                            reader.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * HttpClient请求方式
+     * @param address 请求地址
+     * @param listener 回调接口，用于回传响应的数据
+     */
+    public static void sendRequestWithHttpClient(final String address,final HttpCallbackListener listener) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpGet httpGet = new HttpGet(address);
+                    HttpResponse httpResponse = httpClient.execute(httpGet);
+
+                    if (httpResponse.getStatusLine().getStatusCode() == 200) {
+                        // 请求和响应都成功了
+                        HttpEntity entity = httpResponse.getEntity();
+                        String response = EntityUtils.toString(entity, "utf-8");
+                        if (listener != null) {
+                            listener.onFinish(response);
+                        }
+                    }
+                } catch (Exception e) {
+                    if (listener != null) {
+                        listener.onError(e);
+                    }
+                }
+            }
+        }).start();
+    }
+}
